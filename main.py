194ceb1369ec32778956f5f1f5158d063b8c7186
reyneret <reynerelazaro@gmail.com>
2026-09-07 15:51:38 +0700

    Main Menu: - Added ASCII Art - Added user/admin login feature - Added password hashing for login

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 # ===================================
-# [Your Program Title]
+# NgeFly: Flight Ticket Management System
 # ===================================
-# Developed by. Bayu Prasetya
-# JCDS - [Class Batch]
+# Developed by Reyner Elazaro Tampubolon
+# JCDSBSD - 34
 
 
 # /************************************/
 
+# /===== Library Imports =====/
+import pwinput
+
 # /===== Data Model =====/
-# Create your data model here
-data = [] # Example data model
+# Basic Info, Departure Time, Arrival Time, Departure Place, Arrival Place, Plane, Flight Type, Price
+flightSchedule = [
+    ["Business Flight: Jakarta - Denpasar", " 24 September 2026 07:25", "24 September 2026 11:40", "Jakarta", "Denpasar", "Boeing 747", "Business", 6_500_000]
+]
 
 
 # /===== Feature Program =====/
-# Create your feature program here
 def read():
     """Function for read the data
     """
@@ -34,13 +38,58 @@ def delete():
     """
     return
 
+def adminPanel():
+    print("""
+        =====================================================
+         __    _  _______  _______  _______  ___      __   __ 
+        |  |  | ||       ||       ||       ||   |    |  | |  |
+        |   |_| ||    ___||    ___||    ___||   |    |  |_|  |
+        |       ||   | __ |   |___ |   |___ |   |    |       |
+        |  _    ||   ||  ||    ___||    ___||   |___ |_     _|
+        | | |   ||   |_| ||   |___ |   |    |       |  |   |  
+        |_|  |__||_______||_______||___|    |_______|  |___|  
+        ======================================================
+                           ||ADMIN PANEL||
+    
+        ! You have [n] bookings awaiting approval !
+
+        Menu:
+        1. View flights
+        2. Add flights
+        3. Edit flights
+        4. Cancel flights
+        5. Bookings
+        6. Exit Program
+
+        """)
+    inputUser = input("Select menu number: ")
+
 # /===== Main Program =====/
 # Create your main program here
 def main():
-    """Function for main program
-    """
+    print(f"""
+        =====================================================
+         __    _  _______  _______  _______  ___      __   __ 
+        |  |  | ||       ||       ||       ||   |    |  | |  |
+        |   |_| ||    ___||    ___||    ___||   |    |  |_|  |
+        |       ||   | __ |   |___ |   |___ |   |    |       |
+        |  _    ||   ||  ||    ___||    ___||   |___ |_     _|
+        | | |   ||   |_| ||   |___ |   |    |       |  |   |  
+        |_|  |__||_______||_______||___|    |_______|  |___|  
+        ======================================================
+                 Flight Ticket Management System
+    
+        Welcome, {loginUsername}!
+        
+        Menu:
+        1. View flights
+        2. Search flights
+        3. Book flights
+        4. Exit program
 
-    input_user = input("Insert your option: ")
+        """)
+
+    input_user = input("Select menu number: ")
     if input_user == "1":
         read()
     elif input_user == "2":
@@ -54,4 +103,23 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    print("""
+        =====================================================
+         __    _  _______  _______  _______  ___      __   __ 
+        |  |  | ||       ||       ||       ||   |    |  | |  |
+        |   |_| ||    ___||    ___||    ___||   |    |  |_|  |
+        |       ||   | __ |   |___ |   |___ |   |    |       |
+        |  _    ||   ||  ||    ___||    ___||   |___ |_     _|
+        | | |   ||   |_| ||   |___ |   |    |       |  |   |  
+        |_|  |__||_______||_______||___|    |_______|  |___|  
+        ======================================================
+                 Flight Ticket Management System
+    
+        Login
+        """)
+    loginUsername = input("Enter username: ")
+    loginPassword = pwinput.pwinput(prompt="Enter password: ", mask="*")
+    if loginUsername == "Admin01" and loginPassword == "Admin123":
+        adminPanel()
+    else:
+        main()
